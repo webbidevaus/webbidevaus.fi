699170bbfdff8442b448764d00bc8ab360cafc11
Riku Rouvila <riku.rouvila@gmail.com>
2018-11-17 14:27:12 +0200

    remove inline styles from layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -27,17 +27,7 @@ const Layout = ({ children }) => (
         >
           <html lang="en" />
         </Helmet>
-
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
+        <div>{children}</div>
       </>
     )}
   />
