bd3c57c505c5ad9bce65ca2e4b3a28343dd20253
Riku Rouvila <riku.rouvila@gmail.com>
2018-11-17 14:20:18 +0200

    remove header from layout, add first header styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
-import Header from './header'
 import './layout.css'
 
 const Layout = ({ children }) => (
@@ -28,7 +27,7 @@ const Layout = ({ children }) => (
         >
           <html lang="en" />
         </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
+
         <div
           style={{
             margin: '0 auto',
