36f4999ae4aec6c35651f2eb54438827b6c1aae3
Antti Mattila <anttti@fastmail.fm>
2018-12-02 10:06:50 +0200

    Update site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Webbidevaus.fi',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
