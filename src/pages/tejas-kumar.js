a6c2d6165cac7a0e65952629c3eb4a7b7dded907
Riku Rouvila <riku.rouvila@gmail.com>
2019-06-27 17:54:14 +0300

    add one-off page for Tejas's interview

---
+++ src/pages/tejas-kumar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Episode from '../episodes/template'
+
+const episode = {
+  title: 'React Finland - Interview with Tejas Kumar',
+  number: 47,
+  publishedAt: new Date('2019-06-17T14:47:42.458Z'),
+  longDescription: `
+  ### Tejas Kumar **[@TejasKumar_](https://twitter.com/tejaskumar_)**
+
+  Talk: [Scalable Design Systems with TypeScript](https://www.youtube.com/watch?v=ZsBW4S8hYMU)
+
+  **Linkit**
+
+  - [https://github.com/contiamo/operational-ui](https://github.com/contiamo/operational-ui)
+  `,
+  duration: 1061,
+  audioFile: {
+    url: 'tejas.mp3',
+  },
+}
+
+const Page = () => <Episode pageContext={episode} />
+
+export default Page
