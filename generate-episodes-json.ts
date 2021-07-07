a7d040780a322400ddbfcfa9c748d0f689a3a2bf
Riku Rouvila <riku.rouvila@gmail.com>
2021-07-07 15:06:08 +0300

    use atx headers for new episodes

--- generate-episodes-json.ts
+++ generate-episodes-json.ts
@@ -16,15 +16,18 @@ async function main() {
     episodeRequests.map((request) => MaybeAsync.fromPromise(() => request))
   );
 
-  const episodesWithMarkdownDescription = fullEpisodeDetails.map((episode) => {
-    // Episodes before 55 have description in markdown, others are in HTML
-    return {
-      ...episode,
-      long_description: new TurndownService().turndown(
-        episode.long_description
-      ),
-    };
-  });
+  const episodesWithMarkdownDescription = fullEpisodeDetails
+    .slice(0, 1)
+    .map((episode) => {
+      // Episodes before 55 have description in markdown, others are in HTML
+
+      return {
+        ...episode,
+        long_description: new TurndownService({ headingStyle: "atx" }).turndown(
+          episode.long_description
+        ),
+      };
+    });
 
   writeFileSync(
     "public/episodes.json",
